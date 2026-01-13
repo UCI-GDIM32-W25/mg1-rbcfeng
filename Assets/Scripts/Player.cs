1710ec24713027cd804bfeb36040872e344c9dd2
rbcfeng <rebeccf2@uci.edu>
2026-01-12 17:37:25 -0800

    Finished MG1

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -13,16 +13,42 @@ public class Player : MonoBehaviour
 
     private void Start ()
     {
-        
+        _numSeedsLeft = _numSeeds;
     }
 
     private void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space) && _numSeedsLeft > 0)
+        {
+            PlantSeed();
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            _playerTransform.Translate(Vector3.up * _speed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            _playerTransform.Translate(Vector3.down * _speed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            _playerTransform.Translate(Vector3.left * _speed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            _playerTransform.Translate(Vector3.right * _speed * Time.deltaTime);
+        }
     }
 
     public void PlantSeed ()
     {
-        
+       Instantiate(_plantPrefab, _playerTransform.position, Quaternion.identity);
+         _numSeedsPlanted++;
+         _numSeedsLeft = _numSeeds - _numSeedsPlanted;
+       _plantCountUI.UpdateSeeds(_numSeedsLeft, _numSeedsPlanted);
     }
 }
